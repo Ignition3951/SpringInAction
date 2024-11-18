819c110eaa9e0e458e7f93a997bd72fca3869d15
Utkarsh <ignition3951@gmail.com>
2024-11-18 12:23:19 +0530

    Conflict removal done maually

--- src/main/java/com/utk/config/ProjectConfig.java
+++ src/main/java/com/utk/config/ProjectConfig.java
@@ -1,8 +1,41 @@
 package com.utk.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.utk.model.Parrot;
 
 @Configuration
 public class ProjectConfig {
 
+	@Bean
+	Parrot parrot1() {
+		Parrot parrot = new Parrot();
+		parrot.setName("PArrot 1");
+		return parrot;
+	}
+
+	@Bean
+	Parrot parrot2() {
+		Parrot parrot = new Parrot();
+		parrot.setName("PArrot 2");
+		return parrot;
+	}
+
+	@Bean
+	Parrot parrot3() {
+		Parrot parrot = new Parrot();
+		parrot.setName("PArrot 3");
+		return parrot;
+	}
+
+	@Bean
+	String value() {
+		return "Value returned!!!";
+	}
+
+	@Bean
+	Integer number() {
+		return 13;
+	}
 }
--- src/main/java/com/utk/main/Main.java
+++ src/main/java/com/utk/main/Main.java
@@ -1,12 +1,9 @@
 package com.utk.main;
 
-import java.util.function.Supplier;
-
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.utk.config.ProjectConfig;
-import com.utk.model.Parrot;
 
 public class Main {
 
@@ -14,15 +11,6 @@ public class Main {
 
 		try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
 				ProjectConfig.class)) {
-			Parrot parrot = new Parrot();
-			parrot.setName("miki");
-
-			Supplier<Parrot> supplierParrot = () -> parrot;
-
-			applicationContext.registerBean("miki", Parrot.class, supplierParrot);
-			Parrot registeredParrot = applicationContext.getBean(Parrot.class);
-			System.out.println(registeredParrot.getName());
-
 		} catch (BeansException e) {
 			e.printStackTrace();
 		}
