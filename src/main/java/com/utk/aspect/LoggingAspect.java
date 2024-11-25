f8565e895df3a28b3aab92b0bd6993b85d390fff
Utkarsh <ignition3951@gmail.com>
2024-11-25 15:41:55 +0530

    Manipulate method using aspect

--- src/main/java/com/utk/aspect/LoggingAspect.java
+++ src/main/java/com/utk/aspect/LoggingAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import com.utk.model.Comment;
+
 @Aspect
 public class LoggingAspect {
 
@@ -19,7 +21,11 @@ public class LoggingAspect {
 			Object[] methodArguments = joinPoint.getArgs();
 			logger.info("Method will execute having name : " + methodName + " and arguments : "
 					+ Arrays.asList(methodArguments));
-			Object returnedValue = joinPoint.proceed();
+			Comment aspectComment = new Comment();
+			aspectComment.setAuthor("Aspect");
+			aspectComment.setText("Comment by aspect");
+			Object[] newArguments = { aspectComment };
+			Object returnedValue = joinPoint.proceed(newArguments);
 			logger.info("Method executed and returned : " + returnedValue);
 			return returnedValue + " updated by Aspect";
 		} catch (Throwable e) {
